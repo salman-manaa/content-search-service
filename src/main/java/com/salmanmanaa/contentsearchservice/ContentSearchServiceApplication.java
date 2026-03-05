116b3c8b38aa406cb370296a3f111d9db03f6146
salman <salman.h.manaa@gmail.com>
2026-03-06 00:37:18 +0100

    chore: initialize Spring Boot content search service

---
+++ src/main/java/com/salmanmanaa/contentsearchservice/ContentSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.salmanmanaa.contentsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salmanmanaa/contentsearchservice/ContentSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salmanmanaa.contentsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
